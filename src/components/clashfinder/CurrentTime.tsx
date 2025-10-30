3d0f485456002d5fe2529d44c92a7fb9120dd0b7
Ivan Ho <ivandaho@gmail.com>
2025-10-30 16:46:28 +0800

    fix: CurrentTime z-index

--- src/components/clashfinder/CurrentTime.tsx
+++ src/components/clashfinder/CurrentTime.tsx
@@ -3,18 +3,20 @@ type CurrentTimeProps = {
   minTime: number;
 };
 
+const offset = -10; // huh ???
+
 const CurrentTime = ({ pixelsPerMinute, minTime }: CurrentTimeProps) => {
   const d = new Date();
   let dHours = d.getHours();
   const dMinutes = d.getMinutes() + 5;
   const minutes = dHours * 60 + dMinutes;
-  const pos = (minutes - minTime) * pixelsPerMinute + 60;
+  const pos = (minutes - minTime) * pixelsPerMinute + 60 + offset;
 
   if (pos < 0) return null;
 
   return (
     <div
-      className="relative left-0 right-0 border-t border-white/50 flex justify-center z-12"
+      className="relative left-0 right-0 border-t border-white/50 flex justify-center z-10"
       style={{ top: `${pos}px`, height: 0 }}
     ></div>
   );
